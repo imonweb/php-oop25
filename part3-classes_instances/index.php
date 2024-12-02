d3b6d080fc731644a09737d12776c3001097ea28
Imon <contact@imonweb.co.uk>
2024-12-02 01:44:34 +0000

    class

---
+++ part3-classes_instances/index.php
@@ -0,0 +1,30 @@
+<?php 
+
+/*
+================================================
+   Classes & Intantiation 
+================================================
+*/
+
+// bluepritn / mold / pattern / factory for objects
+class Product {
+
+  // properties = variable
+  public $price = 2;
+  public $color = "green";
+  public $total = 0;
+
+  // method - function 
+  function calculate_total() {
+
+  }
+}
+
+$book = new Product(); // instantiation
+// echo "book $" . $book->price;
+$book->price = 10;
+
+echo "<br>";
+
+$phone = new Product(); // instantiation
+echo "phone $" . $phone->price;
